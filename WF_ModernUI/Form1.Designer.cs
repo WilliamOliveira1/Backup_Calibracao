c3e81afe2bd1d1bf00418928edccf7dcd1c5ea38
William Oliveira <1704639@aluno.univesp.br>
2020-08-07 20:16:24 -0300

    First commit
    Gerado janela para criar backup de arquivo, inicio da aplicação.

---
+++ WF_ModernUI/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WF_ModernUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelCabecalho = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnFechar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panelCabecalho.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelCabecalho
+            // 
+            this.panelCabecalho.BackColor = System.Drawing.Color.DarkCyan;
+            this.panelCabecalho.Controls.Add(this.label1);
+            this.panelCabecalho.Controls.Add(this.BtnFechar);
+            this.panelCabecalho.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelCabecalho.Location = new System.Drawing.Point(0, 0);
+            this.panelCabecalho.Name = "panelCabecalho";
+            this.panelCabecalho.Size = new System.Drawing.Size(518, 47);
+            this.panelCabecalho.TabIndex = 0;
+            this.panelCabecalho.Paint += new System.Windows.Forms.PaintEventHandler(this.panelCabecalho_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(375, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Backup arquivo calibração";
+            // 
+            // BtnFechar
+            // 
+            this.BtnFechar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnFechar.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.BtnFechar.FlatAppearance.BorderSize = 0;
+            this.BtnFechar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BtnFechar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.IndianRed;
+            this.BtnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnFechar.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnFechar.Location = new System.Drawing.Point(415, 4);
+            this.BtnFechar.Name = "BtnFechar";
+            this.BtnFechar.Size = new System.Drawing.Size(91, 40);
+            this.BtnFechar.TabIndex = 0;
+            this.BtnFechar.Text = "Fechar";
+            this.BtnFechar.UseVisualStyleBackColor = true;
+            this.BtnFechar.Click += new System.EventHandler(this.BtnFechar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(359, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 38);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Confirmar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(5, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Escolha a jiga";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(151, 77);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(202, 30);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 135);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panelCabecalho);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormMenu";
+            this.panelCabecalho.ResumeLayout(false);
+            this.panelCabecalho.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelCabecalho;
+        private System.Windows.Forms.Button BtnFechar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ WF_ModernUI/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_ModernUI
+{
+    public partial class Form1 : Form
+    {
+        string nomeComputador = Environment.MachineName;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.button1.Enabled = false;
+            comboBox1.Items.Add(nomeComputador);
+        }
+
+        private void BtnFechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void panelCabecalho_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.Text.Length > 0)
+            {
+                this.button1.Enabled = true;
+            }
+            if (comboBox1.Text.Length == 0)
+            {
+                this.button1.Enabled = false;
+            }
+
+            switch (comboBox1.Text)
+            {
+                case "DESKTOP-S4H6C5I":
+                    try
+                    {
+                        DateTime dataAgora = DateTime.Now;
+                        string data = String.Format("{0:dd_MM_yyyy}", dataAgora);
+                        string path = "d:\\" + data; //nome do diretorio a ser criado
+                        if (Directory.Exists(path))
+                        {
+                            MessageBox.Show("Pasta de backup ja existente");
+                        }
+                        if (!Directory.Exists(path))
+                        {
+                            //Criamos um com o nome folder
+                            Directory.CreateDirectory(path);
+                            File.Copy(@"C:\arquivo.txt", "D:\\" + data + "\\arquivo.txt");
+                            MessageBox.Show("Arquivo de calibração da jiga WFFT01 copiado");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Erro ao gerar backup!");
+                        }
+                    }catch
+                    {
+                        MessageBox.Show("erro!");
+                    }
+                    break;
+                case "WFFT02":
+                    MessageBox.Show("WFFT02");
+                    break;
+                case "WFFT03":
+                    MessageBox.Show("WFFT03");
+                    break;
+                case "WFFT04":
+                    MessageBox.Show("WFFT04");
+                    break;
+                case "WFFT05":
+                    MessageBox.Show("WFFT05");
+                    break;
+                case "WFFT06":
+                    MessageBox.Show("WFFT06");
+                    break;
+                case "WFFT07":
+                    MessageBox.Show("WFFT07");
+                    break;
+                case "WFFT12":
+                    MessageBox.Show("WFFT12");
+                    break;
+                case "WFFT13":
+                    MessageBox.Show("WFFT13");
+                    break;
+                case "WFFT15":
+                    MessageBox.Show("WFFT15");
+                    break;
+                case "WFFT16":
+                    MessageBox.Show("WFFT16");
+                    break;
+                case "FOQM":
+                    MessageBox.Show("FOQM");
+                    break;
+                case "FOQM01":
+                    MessageBox.Show("FOQM01");
+                    break;
+                case "FOQM02":
+                    MessageBox.Show("FOQM02");
+                    break;
+                default:
+                    MessageBox.Show("Selecione uma jiga valida");
+                    break;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {           
+            if (comboBox1.Text.Length > 0)
+            {
+                this.button1.Enabled = true;
+            }
+        }
+    }
+}
